1f9aa8f06281855641af149f64a825b6a901e2d8
SalkCoding <josangbeom1104@gmail.com>
2025-03-27 23:53:38 +0900

    Init commit.

---
+++ src/main/java/com/cm2/Cm2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.cm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cm2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cm2BackendApplication.class, args);
+    }
+
+}
