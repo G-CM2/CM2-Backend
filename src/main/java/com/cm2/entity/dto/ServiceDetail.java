de714ee9c294ec2d2f5b697d256da7093d5d9b01
dogsub <ehdtjq0909@gmail.com>
2025-06-13 03:37:53 +0900

    [feat] Service 관련 DTO 추가 작성

---
+++ src/main/java/com/cm2/entity/dto/ServiceDetail.java
@@ -0,0 +1,18 @@
+package com.cm2.entity.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record ServiceDetail(
+        String id,
+        String name,
+        String mode,
+        int replicasDesired,
+        int replicasRunning,
+        String image,
+        List<String> constraints,
+        Resources resources,
+        List<TaskStatus> tasks
+) {}
